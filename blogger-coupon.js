488ffec8a164cdee44a28d2e8c420d054faa2ac6
Da Ric <darit.ly@gmail.com>
2021-11-16 09:21:00 +0700

    update

---
+++ blogger-coupon.js
@@ -0,0 +1,147 @@
+﻿//required moment.js
+let tokenAT = $("#tokenAT").val();
+let counponList = $("#counponList");
+let mggLoading = $("#counponLoading");
+let titleCoupon = $("#titleCoupon");
+let btnMoreCoupon = $("#btnMoreCoupon");
+
+var loadingImg = "https://blogger.googleusercontent.com/img/a/AVvXsEjem-tWEY77eArEVzlz0a7aSkuZaCfl9envYMXZk4xKHb0bEHrXEf2LtZ8kI6hlbl2zOcu7YIQiYHXjbmIvAmKcxO8B7kpQlm0_UyJeuhhUxVevCr8NEfHMAOrv7L-bMP1s9Zta0C7Nyjz2bE_4r0jRamiYy2222AWWqc0JyN92AxBvBOe5DJEqB4yB=s320";
+var hostCounpon = 'https://api.accesstrade.vn/v1/offers_informations';
+//let currentPage = 1, limit = 50, merchant = "shopee";
+
+//getCounpon("shopee");
+
+btnMoreCoupon.click(function () {
+	currentPage++; getCounpon(merchant, $("#categoriesCounponFilter").val());
+})
+$('#txtLinkFilter').bind("enterKey", function (e) {
+	$("#btnFilterCoupon").click()
+});
+$('#txtLinkFilter').keyup(function (e) {
+	if (e.keyCode == 13) { $(this).trigger("enterKey"); }
+});
+$("#btnFilterCoupon").click(function () {
+	currentPage = 1; counponList.empty(); searchCounpon($("#txtLinkFilter").val());
+})
+
+$("#btnCategoriesCounponFilter").click(function () {
+	currentPage = 1; counponList.empty(); getCounpon(merchant, $("#categoriesCounponFilter").val());
+})
+$("#categoriesCounponFilter").change(function () {
+	currentPage = 1; counponList.empty(); getCounpon(merchant, $("#categoriesCounponFilter").val());
+})
+
+function getCounpon(merchant, categories) {
+	btnMoreCoupon.show();
+	let authorizationToken = 'Token ' + tokenAT;
+	let url = hostCounpon +'?merchant='+ merchant + '&status=1&page=' + currentPage + '&limit=' + limit;
+	if (categories) { url += '&categories=' + categories; }
+	mggLoading.html(`<p style="color:green;text-align:center;background:#fff;margin-top:10px"><img src="` + loadingImg + `" style="width:200px;display:block;margin:auto" alt="đang chờ"/><br/> <b>Đang tải dữ liệu....</b> </p>`);
+	$.ajax({
+		type: "GET",
+		beforeSend: function (request) { request.setRequestHeader("Authorization", authorizationToken); },
+		url: url,
+		data: null,
+		processData: false,
+		success: function (res) {
+			mggLoading.empty();
+			let data = res.data;
+			appendItem(data);
+		}
+	});
+}
+
+function searchCounponShopee(urlLink) {
+	let authorizationToken = 'Token ' + tokenAT;
+	counponList.empty();
+	let url = hostCounpon + '/coupon?url=' + urlLink;
+	mggLoading.html(`<p style="color:green;text-align:center;background:#fff;margin-top:10px"><img src="` + loadingImg+`" style="width:200px;display:block;margin:auto" alt="đang chờ"/><br/> <b>Đang tải dữ liệu....</b> </p>`);
+	$.ajax({
+		type: "GET",
+		beforeSend: function (request) { request.setRequestHeader("Authorization", authorizationToken); },
+		url: url,
+		data: null,
+		processData: false,
+		success: function (res) {
+			mggLoading.empty();
+			let dataAll = res.data;
+			if (dataAll.length > 0) {
+				titleCoupon.html('<h4>Mã giảm giá có thể áp dụng:</h4>');
+				var data = [];
+				let endCol = 4;
+				if (dataAll.length < endCol) {
+					endCol = dataAll.length;
+				}
+				for (let index = 0; index < endCol; index++) { data.push(dataAll[index]); }
+				appendItem(data);
+			} 
+		}
+	});
+}
+function appendItem(data) {
+	$.each(data, function (i, item) {
+		var then = new Date(item.end_time);
+		then.setUTCHours(23, 59, 59, 999);
+		var now = new Date(); 
+		var ms = moment(then, "DD/MM/YYYY HH:mm:ss").diff(moment(now, "DD/MM/YYYY HH:mm:ss"));
+		var d = moment.duration(ms);
+		var s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
+		let hour = d.asHours().toFixed(0);
+		let hsd = "<b style='color:green'> " + " còn  " + hour + " giờ nữa</b>";
+		if (then == now) {
+			hsd = "<b style='color:green'> " + " còn  " + (24 - now.hour).toFixed(0) + " giờ </b>";
+		}
+		if (hour > 24) {
+			let ngay = hour / 24;
+			hsd = "<b style='color:green'> " + " còn  " + ngay.toFixed(0) + " ngày</b>";
+		}
+		if (hour == 0) {
+			if (d.asMinutes() <= 0) {
+				hsd = "<b  style='color:red'> " + " hết hạn</b>";
+			}
+			else {
+				hsd = "<b  style='color:orange'> " + " Còn " + d.asMinutes().toFixed(0) + " phút</b>";
+			}
+		}
+		let coupon_code = '';
+		if (item.coupons.length > 0) { 
+			coupon_code = `<p class="p-coupon-code"> <i class="fa fa-tag mr-2"></i> Mã Coupon:</p> <div class="coupon-code"> <div class="coupon-code--key" id="`+ item.id + `">` + (item.coupons[0] ? item.coupons[0].coupon_code : '') + `</div> <button class="btn-copy btn-copy-coupon"  type="button" onclick="copyCodeOpenLink( '` + item.aff_link + `','` + item.id + `',)"> 	<i class="fa fa-copy"></i></button> </div> `;
+
+			counponList.append(` <div class="coupon-card coupon-item-card">
+												<div class="coupon-info coupon-name">
+													<div class="mr-3 coupon-image" style="width:50px">
+													<img alt="" src="`+ item.image + `""></div>
+													<p class="coupon-name cursor-pointer"> `+ item.name + ` </p></div>
+													<div class="coupon-content">
+													<div class="circle circle-right"></div>
+													<div class="circle circle-left"></div>
+													 `+ coupon_code + `
+													<div class="coupon-expired">
+													<p class="coupon-due-date"><i class="fa fa-clock mr-2"></i> HSD: <b  >`+ item.end_time + `</b></p>
+													<p class="item-time-left"> <i  class="fa fa-hourglass-half  mr-2"></i> Thời gian: `+ hsd + ` </p><p></p></div></div>
+													<div class="coupon-action mt-4">
+													<a class="cursor-pointer link-coupon-action" data-bs-toggle="collapse" href="#collapseContent`+ item.id + `" role="button" aria-expanded="false" aria-controls="collapseContent` + item.id + `">Chi tiết</a>
+													<a class="cursor-pointer link-coupon-action"  href='`+ item.aff_link + `' target="_blank">Lấy link</a> </div>
+													<div class="collapse" id="collapseContent`+ item.id + `"> <div class="card card-body">  `+ item.content + `  </div> </div> </div>` );
+		}
+	});
+}
+function copyText(element) {
+	var range, selection, worked; 
+	if (document.body.createTextRange) {
+		range = document.body.createTextRange();
+		range.moveToElementText(element);
+		range.select();
+	} else if (window.getSelection) {
+		selection = window.getSelection();
+		range = document.createRange();
+		range.selectNodeContents(element);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	} 
+	try { document.execCommand('copy'); alert('đã copy mã'); }
+	catch (err) { alert('unable to copy text'); }
+}
+function copyCodeOpenLink(link, id) { 
+	var copyTextEl = document.getElementById(id); copyText(copyTextEl); if (link) { setTimeout(window.open(link), 900); }
+}
---
+++ blogger-libraries.js
@@ -0,0 +1,53 @@
+﻿let homePageUrl = getUrlHost();
+
+function getUrlHost() { let current = location.href; current = current.replaceAll("https://", ""); if (current.indexOf("?") > -1) { current = current.split('?')[0]; } if (current.indexOf("/") > -1) { current = current.split('/')[0]; } return "https://" + current; }
+
+function getLinkImageFromContent(content) { let s = content; let a = s.indexOf('<img'); let b = s.indexOf('src="', a); let c = s.indexOf('"', b + 5); return s.substr(b + 5, c - b - 5); }
+
+function getLinkFromLinks(links) { for (let i = 0; i < links.length; i++) { if (links[i].type == "text/html") { return links[i].href.replaceAll("#comment-form", ""); } } return ""; }
+
+function getPostId() { let atom = document.querySelectorAll("meta[property='postId']"); for (let i = 0; i < atom.length; i++) { let id = atom[i].getAttribute("content"); if (id) { return id; } } return "0"; }
+
+function getValueCodeFromContent(content, beginCode, endCode) { return (-1 != content.indexOf(beginCode) ? content.substring(content.indexOf(beginCode) + beginCode.length, content.indexOf(endCode)) : ""); }
+
+function getLabelFromLink() {
+	let str = location.href; let urllength = homePageUrl.length; let label = ""; if (-1 != str.indexOf("search/label")) {
+		if (-1 != str.indexOf("?")) { var str1 = str.split("?")[0]; label = str1.substring(urllength + 13, str1.length); } else label = str.substring(urllength + 13, str.length);
+	}
+	return label;
+}
+
+
+function getItemProductFromContent(content) {
+	let linkImage = getValueCodeFromContent(content, "[thumbnail_url]", "[/thumbnail_url]");
+	let id = getValueCodeFromContent(content, "[id]", "[/id]");
+	let giaban = getValueCodeFromContent(content, "[price]", "[/price]");
+	let chitiet = getValueCodeFromContent(content, "[description]", "[/description]");
+	let mota = getValueCodeFromContent(content, "[short_description]", "[/short_description]");
+	let list_price = getValueCodeFromContent(content, "[list_price]", "[/list_price]");
+	let original_price = getValueCodeFromContent(content, "[original_price]", "[/original_price]");
+	let discount = getValueCodeFromContent(content, "[discount]", "[/discount]");
+	let discount_rate = getValueCodeFromContent(content, "[discount_rate]", "[/discount_rate]");
+	let url_path = getValueCodeFromContent(content, "[url_path]", "[/url_path]");
+	let publisher = getValueCodeFromContent(content, "[publisher]", "[/publisher]");
+	let manufacturer = getValueCodeFromContent(content, "[manufacturer]", "[/manufacturer]");
+	let publication_date = getValueCodeFromContent(content, "[publication_date]", "[/publication_date]");
+	let dimensions = getValueCodeFromContent(content, "[dimensions]", "[/dimensions]");
+	let book_cover = getValueCodeFromContent(content, "[book_cover]", "[/book_cover]");
+	let number_of_page = getValueCodeFromContent(content, "[number_of_page]", "[/number_of_page]");
+	let badges_new = getValueCodeFromContent(content, "[badges_new]", "[/badges_new]");
+	let current_seller = getValueCodeFromContent(content, "[current_seller]", "[/current_seller]");
+	let seller_specifications = getValueCodeFromContent(content, "[seller_specifications]", "[/seller_specifications]");
+	let other_sellers = getValueCodeFromContent(content, "[other_sellers]", "[/other_sellers]");
+	let url_key = getValueCodeFromContent(content, "[url_key]", "[/url_key]");
+	let dich_gia = getValueCodeFromContent(content, "[dich_gia]", "[/dich_gia]");
+	let authors = getValueCodeFromContent(content, "[authors]", "[/authors]");
+	let rating_average = getValueCodeFromContent(content, "[rating_average]", "[/rating_average]");
+	let review_count = getValueCodeFromContent(content, "[review_count]", "[/review_count]");
+	let link = getLinkFromLinks(n.link);
+	link = link.replaceAll("#comment-form", ""); linkImage = linkImage.replaceAll("http://", "https://");
+	let dc = "";
+	if (discount_rate != '0%') { dc = `<span class="discount item_price"> ` + discount_rate + `</span>`; }
+	return (`<div class="item col-md-3"> <div class="product-grid-item simpleCart_shelfItem"> <div class="product_img relative"> <div class="icon_flat"></div> <a href = "` + link + `"> <img alt="` + title + `"  data-src="` + linkImage + `" class = "lazy" ></a> </div> <div class="product_title"> <h3><a href="` + link + `" class="item_name">` + title + `</a></h3> </div> <div class="product_price"> <span class="price_new item_price"> ` + giaban.replaceAll("₫", "") + ` đ</span> ` + dc + ` </div>  ` + badges_new + ` <div class="area_btn"> </div></div></div>`);
+
+}
