08b72d7d59ab220a9b99f0e267de5557ba72718e
Da ric <lychanhdaric@gmail.com>
2022-05-12 15:41:29 +0700

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+let run =100;
+$("#btnGenerate").click(  function(){  
+  //==================================
+  for(let i=1;i<200;i++){ 
+     setTimeout(function(){
+       let rs = getRndInteger(parseInt($("#txtMin").val()) , parseInt($("#txtMax").val()));
+      $("#rs1").empty();  
+       $("#rs1").append(rs); 
+        $("#rs2").empty();  
+       $("#rs2").append(rs); 
+     }, 50); 
+  } 
+    //==================================
+  for(let i=1;i<50;i++){
+     setTimeout(function(){
+        let rs = getRndInteger(parseInt($("#txtMin").val()) , parseInt($("#txtMax").val()));
+        $("#rs1").empty();  
+       $("#rs1").append(rs); 
+        $("#rs2").empty();  
+       $("#rs2").append(rs); 
+     }, (i * 200));  
+    
+  } 
+  
+ })
